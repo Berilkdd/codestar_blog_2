6b3f883790502d4baafb1c11c6762bea1b367e03
Berilkdd <brlkrdnz@hotmail.com>
2026-05-21 12:58:34 +0100

    add base html and custom css

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
 
 # Create your views here.
